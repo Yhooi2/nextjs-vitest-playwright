2647ae7493b27c0bf74707d1e867bbc05c349c99
Art <artyhoo@icloud.com>
2025-09-07 20:56:46 +0300

    start

---
+++ vitest.global.setup.ts
@@ -0,0 +1,16 @@
+// import cleanupTestDatabase from '@/utils/__tests__/utils/cleanup-test-database';
+
+// Выполняется один раз до (setup) и после (tearDown) всего
+// набора тестов
+
+export async function setup() {
+  // Запускается перед всеми тестами
+  // Это немного избыточно, но иногда тест выполняется не полностью
+  // и оставляет мусор, как старые базы данных или данные в таблице
+//   await cleanupTestDatabase();
+}
+
+export async function teardown() {
+  // Запускается после всех тестов
+//   await cleanupTestDatabase();
+} 
